fc34650c472e64735ac8cdf599e25af313c420f6
azusa152 <iikuboazusa@gmail.com>
2017-05-27 14:54:40 +0900

    fiert virsion
    fiert virsion

--- main.cpp
+++ main.cpp
@@ -3,32 +3,40 @@
 #include <sstream>
 #include <string>
 #include <iomanip>
+#include <cmath>
 using namespace std;
 
-#define ORIGINALFILENAME ".\\data\\real.txt"
-#define COMPRESSFILENAME ".\\data\\real_test(2,4).txt"
+#define ORIGINALFILENAME ".\\data\\slow_up.txt"
+#define COMPRESSFILENAME ".\\data\\slow_up(2,4).txt"
+
 float original_data_array[1000];
 int original_data_quantity=0;
+
 string compress_data_array[1000];
 int compress_data_quantity=0;
+
+float reduction_array[1000];
+int reduction_data_quantity=0;
 
 const int kMeanQuantity=2;
 const int kGroupQuantity=4;
 
-float mean_array[1000];
-int mean_counter=0;
+float error_rate=0;
 
-string SAX_array[1000];
-int SAX_counter=0;
 const double kSAXRange=0.5;
 
 void InputData();
-void SAXtoFloat();
+void SAXReduction();
+float SymbolToFloat(char symbol,int valueStandard);
+void ErrorRate();
+void Reduction(float value);
 
 int main()
 {
     InputData();
-    SAXtoFloat();
+    SAXReduction();
+    ErrorRate();
+
     return 0;
 }
 
@@ -52,19 +60,65 @@ void InputData(){
     g.close();
 }
 
-void SAXtoFloat(){
+void SAXReduction(){
+
+    float valueStandard=0;
     for(int i=1;i<=compress_data_quantity;i++){
             stringstream ss;
             ss<<compress_data_array[i];
             float v;
             ss>>v;
 
-            cout << v<< endl;
-        if(i%kGroupQuantity==0){
+            if(v!=0){
+                Reduction(v);
+                valueStandard=v;
+            }
 
-        }
+            else{
+                string a=compress_data_array[i];
+                const char *c=a.c_str();
+                Reduction(SymbolToFloat(c[0],valueStandard));
 
+            }
+
+    }
+}
+void Reduction(float value){
+    for(int i=0;i<kMeanQuantity;i++){
+    reduction_data_quantity++;
+    reduction_array[reduction_data_quantity]=value;
     }
 
 
+}
+
+float SymbolToFloat(char symbol,int valueStandard){
+    float minRange=(kSAXRange*-1)*27;
+    float maxRange=kSAXRange*27;
+
+    for(int i=1;i<=52;i++){
+        if(symbol>=65&&symbol<=90){
+                if(symbol==(64+i)){
+                    return (valueStandard+(-1*i*(kSAXRange/2)));
+                }
+
+        }
+        else if(symbol>=97&&symbol<=122){
+                if(symbol==(96+i)){
+                    return (valueStandard+(i*(kSAXRange/2)));
+                }
+
+        }
+    }
+
+}
+void ErrorRate(){
+    for(int i=1;i<=reduction_data_quantity;i++){
+        error_rate=error_rate+abs(original_data_array[i]-reduction_array[i]);
+        cout<<"original_data_array:"<<original_data_array[i]<<endl;
+        cout<<"reduction_array:"<<reduction_array[i]<<endl;
+        cout<<"------------------"<<endl;
+    }
+
+    cout<<"error:"<<error_rate<<endl;
 }
